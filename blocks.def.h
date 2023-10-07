9a25d3acacf12e85aa60c181d5e55c04de12871b
Jake Ferrari <jakeferrari93@gmail.com>
2023-10-07 15:52:06 -0700

    update blocks config file to include sys scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+  {" 🐧 ",  "/home/jake/.local/bin/dwmblocks/kernel",   360,          2},
+  {" 🔺 ",   "/home/jake/.local/bin/dwmblocks/upt",      60,            2},
+  {" 📦 ",   "/home/jake/.local/bin/dwmblocks/pacupdate",  360,          9},
+  {" 💻 ",   "/home/jake/.local/bin/dwmblocks/memory",  6,               1},
+  {" 🔊 ",   "/home/jake/.local/bin/dwmblocks/volume",  0,               10},
+  {" 🕑 ",   "/home/jake/.local/bin/dwmblocks/clock",  60,               0},
 };
-
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "|";
 static unsigned int delimLen = 5;
